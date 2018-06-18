ab24279fdacc864e420cf1bb830e70dd045872af
zhufangda <zhufangda@gmail.com>
2018-06-18 08:55:10 +0200

    update

--- Carte/src/js/main.js
+++ Carte/src/js/main.js
@@ -1,15 +1,13 @@
-let map_width = 1900
-let map_height = 1800
 let projection = d3.geoAzimuthalEquidistant()
-  .translate([map_width/2, map_height/2])
-  .scale([700]);
+  .translate([800/2, 600/2])
+  .scale([500]);
 
 let path = d3.geoPath(projection)
 
 let svg = d3.select('body')
   .append('svg')
-  .attr('width', map_width)
-  .attr('height', map_height)
+  .attr('width', 800)
+  .attr('height', 600)
 
 console.log('Show info')
 
@@ -22,7 +20,7 @@ d3.json('data/europe.json').then(function (json) {
     .append('path')
     .attr('d', path)
     .attr('name', (d) => d.properties.name)
-    .attr('stroke', 'black')
+  .attr('stroke', 'black')
     .attr('stroke-width', 1)
     .attr('fill', 'white')
     .attr("transform", "translate(0,300)")
