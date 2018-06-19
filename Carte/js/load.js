f27f630e5048610acee76f3e6b7d90ad36870f4a
zhufangda <zhufangda@gmail.com>
2018-06-19 15:10:31 +0200

    update the data and load.js

--- Carte/js/load.js
+++ Carte/js/load.js
@@ -2,9 +2,16 @@ var countries = [];
 var activities = [];
 var dataset = new Map();
 
-//exemple d'accès au dataset : dataset.get("Belgium").get("Sleep").participationTime ; dataset.get("Belgium").get("Sleep").participationRate ; dataset.get("Belgium").get("Sleep").timeSpent
+let activity
 
-d3.csv("data/label.csv", function (label) {
+//exemple d'accès au dataset : dataset.get("Belgium").get("Sleep").participationTime ; dataset.get("Belgium").get("Sleep").participationRate ; dataset.get("Belgium").get("Sleep").timeSpent
+let activity_selector = d3.select('body')
+  .append('select')
+  .attr('id', 'activities')
+  .attr('onchange', 'changeAction')
+
+d3.csv("data/label.csv")
+  .then(label => {
   for (var i = 13; i < 34; i++) {
     countries.push(label[i]["DATASET: Time spent, participation time and participation rate in the main activity by sex and day of the week [tus_00week]"]);
   };
@@ -20,25 +27,29 @@ d3.csv("data/label.csv", function (label) {
     }
     dataset.set(countries[i], map);
   }
+
+
+    d3.csv("data/TimeUseData.csv")
+      .then(data => {
+      for (var i = 0; i < data.length; i++) {
+        var l = data[i]
+        if ((l.DAYSWEEK === "All days of the week") && (l.SEX === "Total")) {
+          if (l.UNIT === "Time spent (hh:mm)") {
+            dataset.get(l.GEO).get(l.ACL00).timeSpent = l.Value;
+          }
+          if (l.UNIT === "Participation time (hh:mm)") {
+            dataset.get(l.GEO).get(l.ACL00).participationTime = l.Value;
+          }
+
+          if (l.UNIT === "Participation rate (%)") {
+            dataset.get(l.GEO).get(l.ACL00).participationRate = l.Value;
+          }
+        }
+      }
+        setSelector()
+        loadMap(activity)
+
+    });
+
+
 });
-
-d3.csv("data/data.csv", function (error, data) {
-  for (var i = 0; i < data.length; i++) {
-    l = data[i]
-    if ((l.DAYSWEEK === "All days of the week") && (l.SEX === "Total") && (l.ACL00 != "Total")) {
-      if (l.UNIT === "Time spent (hh:mm)") {
-        dataset.get(l.GEO).get(l.ACL00).timeSpent = l.Value;
-      }
-      if (l.UNIT === "Participation time (hh:mm)") {
-        dataset.get(l.GEO).get(l.ACL00).participationTime = l.Value;
-      }
-
-      if (l.UNIT === "Participation rate (%)") {
-        dataset.get(l.GEO).get(l.ACL00).participationRate = l.Value;
-      }
-    }
-  }
-  setupGraph();
-});
-
-
