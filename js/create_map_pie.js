d26f8ac94d581611e71b1fcd04f9bde669eb3876
zhufangda <zhufangda@gmail.com>
2018-06-23 22:53:50 +0200

    Add legend and zoom in the map

--- js/create_map_pie.js
+++ js/create_map_pie.js
@@ -1,13 +1,9 @@
 let scaleC
 
-let svg_map = d3.select('#map-container')
-let mapContainer = svg_map
-  .append('g')
-  .attr('id', 'map')
-
-let pieContainer = svg_map
-  .append('g')
-  .attr('id', 'pies')
+let svgMap = d3.select('#map-container')
+let mapContainer = svgMap.append('g').attr('id', 'map')
+let pieContainer = svgMap.append('g').attr('id', 'pies')
+let legendCOntainer = svgMap.append('g').attr('id', 'legend')
 
 let mapData
 let sTimeList = []
@@ -20,7 +16,11 @@ function getActivity () {
 }
 
 function getColor (d) {
-  if (isNaN(pTime[d.properties.NAME])) { return 'white' } else { return scaleC(pTime[d.properties.NAME]) }
+  if (isNaN(pTime[d.properties.NAME])) {
+    return 'white'
+  } else {
+    return scaleC(pTime[d.properties.NAME])
+  }
 }
 
 function getSTColor (d) {
@@ -43,7 +43,9 @@ function getSTColor (d) {
 function getParticipationRateMap (activity) {
   let rateMap = new Map()
   for (let country of countries) {
-    rateMap[country] = parseFloat(dataset.get(country).get(activity).participationRate)
+    rateMap[country] = parseFloat(
+      dataset.get(country).get(activity).participationRate
+    )
   }
   return rateMap
 }
@@ -78,18 +80,19 @@ function getSpentTimeList (activities) {
 
     for (let activity of activities) {
       console.log(country_info.country)
-      let str = String(dataset.get(country_info.country).get(activity).timeSpent)
+      let str = String(
+        dataset.get(country_info.country).get(activity).timeSpent
+      )
 
       let splittedStr = str.split(':')
       let hh = parseInt(splittedStr[0])
       let mm = parseInt(splittedStr[1])
-      let aTime = hh + mm / 60.0
+      let aTime = hh * 60 + mm
       aTime = isNaN(aTime) ? 0 : aTime
-      country_info.values.push(
-        { 'activity': activity,
-          'value': aTime
-        }
-      )
+      country_info.values.push({
+        activity: activity,
+        value: aTime
+      })
 
       country_info.total += aTime
     }
@@ -100,7 +103,7 @@ function getSpentTimeList (activities) {
 }
 
 function loadMap (dataset, activity) {
-  console.log('Start to load map to :', svg_map)
+  console.log('Start to load map to :', svgMap)
   d3.json('data/europe.json', function (error, map_json) {
     mapData = map_json
     initSpList()
@@ -113,10 +116,10 @@ function initSpList () {
     // console.log(countries.includes(countryFeature.properties.NAME))
     if (countries.includes(countryFeature.properties.NAME)) {
       let countryInfo = {
-        'country': countryFeature.properties.NAME,
-        'feature': countryFeature,
-        'total': 0.0,
-        'values': []
+        country: countryFeature.properties.NAME,
+        feature: countryFeature,
+        total: 0.0,
+        values: []
       }
       sTimeList.push(countryInfo)
     }
@@ -125,9 +128,10 @@ function initSpList () {
 
 // put all logic in a nice reusable function
 function createPieMap (activities) {
-//   activities = ['Personal care', 'Other and/or unspecified personal care']
+  //   activities = ['Personal care', 'Other and/or unspecified personal care']
   getSpentTimeList(activities)
   updatePieMapScale(activities)
+  updateLegend()
   // use viewBox attributes instead of width + height
 
   let width = $('#map-container').width()
@@ -135,25 +139,27 @@ function createPieMap (activities) {
 
   console.log('Create map:' + activity, 'width', width, 'height', height)
 
-  const projection = d3.geoMercator()
-  // d3's 'fitSize' magically sizes and positions the map for you
+  const projection = d3
+    .geoMercator()
+    // d3's 'fitSize' magically sizes and positions the map for you
     .fitSize([width, height], mapData)
 
   // this is the function that generates position data from the projection
-  const path = d3.geoPath()
-    .projection(projection)
+  const path = d3.geoPath().projection(projection)
 
   // append country outlines
-  let countries_enter = mapContainer.selectAll('.country')
+  let countries_enter = mapContainer
+    .selectAll('.country')
     .data(mapData.features, d => d.properties.NAME)
     .enter()
     .append('g')
     .attr('class', 'country')
 
-  countries_enter.append('path')
+  countries_enter
+    .append('path')
     .attr('class', 'country_path')
     .attr('d', path)
-    .attr('name', (d) => d.properties.NAME)
+    .attr('name', d => d.properties.NAME)
     .attr('stroke', 'black')
     .attr('stroke-width', 1)
     .attr('fill', d => getSTColor(d))
@@ -161,7 +167,8 @@ function createPieMap (activities) {
   //   countries_enter.append('title')
   //     .text((d) => pRateTips(d))
 
-  mapContainer.selectAll('.country_path')
+  mapContainer
+    .selectAll('.country_path')
     .data(mapData.features, d => d.properties.NAME)
     .transition()
     .duration(1000)
@@ -170,82 +177,154 @@ function createPieMap (activities) {
   //   countries_enter.append('title')
   //     .text((d) => pRateTips(d))
 
-  mapContainer.selectAll('.country')
+  mapContainer
+    .selectAll('.country')
     .data(mapData.features, d => d.properties.NAME)
     .transition()
     .duration(1000)
     .attr('fill', d => getSTColor(d))
 
-    /** ************** Draw pie   ************************/
-  let pie = d3.pie()
-    .value(d => {
-      console.log('pie_layout', d)
-      return d.value
-    })
-
-  let outerRadius = 50
+  /** ************** Draw pie   ************************/
+  let pie = d3.pie().value(d => {
+    console.log('pie_layout', d)
+    return d.value
+  })
 
   // Easy colors accessible via a 10-step ordinal scale
 
-  var arc = d3.arc()
+  var arc = d3
+    .arc()
     .innerRadius(0)
     .outerRadius(d => d.data.radius)
 
-  let points = pieContainer.selectAll('.pie')
+  // var arc = d3.arc()
+  //   .innerRadius(5)
+  //   .outerRadius(10)
+  // // Add new pies
+  let points = pieContainer
+    .selectAll('.pie')
     .data(sTimeList, d => d.country)
     .enter()
     .append('g')
     .attr('class', 'pie')
-    .attr('transform', (d) => {
+    .attr('country', d => d.country)
+    .attr('transform', d => {
       const centroid = path.centroid(d.feature)
       return `translate(${centroid[0]}, ${centroid[1]})`
     })
 
-  let pies = points.selectAll('.pie')
-    .data(d => {
-      console.log('pies', d)
-      return pie(d.values)
-    }).enter()
-    .append('g')
-    .attr('class', 'arc')
+  points = pieContainer.selectAll('.pie').data(sTimeList, d => d.country)
 
-  pies.append('path')
-    .attr('d', arc)
+  let allPies = points.selectAll('.arc').data(d => {
+    d.values.map(t => {
+      t.radius = (0.618 * Math.sqrt(path.area(d.feature))) / 2
+      return d
+    })
+    return pie(d.values)
+  }, d => d.data.activity)
+
+  allPies
+    .enter()
+    .append('path')
+    .attr('title', d => d.data.activity)
+    .attr('class', 'arc')
+    .attr('fill', d => pieScale(d.data.activity))
+    .transition()
+    .duration(500)
+    .attrTween('d', d => arcTween(d, arc))
+  allPies
+    .enter(d => d.data.activitys)
+    .append('title')
+    .text(d => d.data.activity)
+
+  allPies
     .attr('fill', function (d, i) {
-      console.log('pie_color', d, d.data.activity, pieScale(d.data.activity))
+      console.log('fill new comer', d)
       return pieScale(d.data.activity)
     })
+    .transition()
+    .duration(500)
+    .attrTween('d', d => arcTween(d, arc))
 
-  points = pieContainer.selectAll('.pie')
-    .data(sTimeList, d => d.country)
-    .attr('transform', (d) => {
-      const centroid = path.centroid(d.feature)
-      return `translate(${centroid[0]}, ${centroid[1]})`
-    })
+  //
+  allPies.exit().remove()
+  //     .transition()
+  //     .duration(1000)
+  //     .attr('fill', function (d, i) {
+  //       return pieScale(d)
+  //     })
+  //     .attrTween('d', d => arcTween(d, arc))
 
-  pies = points.selectAll('.pie')
-    .data(d => {
-      let bounds = path.bounds(d.feature)
-      let max1 = Math.min(Math.abs(bounds[0][0] - bounds[1][0]), Math.abs(bounds[0][1] - bounds[1][1]))
-      let radius = Math.sqrt(path.area(d.feature))
-      console.log('projection', radius, max1, path.area(d.feature), d.feature.properties.NAME)
-      d.values.map(d => {
-        d.radius = 0.618 * radius / 2
-        return d
-      })
+  //   let pies = points.selectAll('.pie')
+  //     .data(d => {
+  //       console.log('pies', d)
+  //       return pie(d.values)
+  //     }).enter()
+  //     .append('g')
+  //     .attr('class', 'arc')
 
-      return pie(d.values)
-    }).enter()
-    .append('g')
-    .attr('class', 'arc')
+  //   pies.append('path')
+  //     .attr('d', arc)
+  //     .attr('fill', function (d, i) {
+  //       console.log('pie_color', d, d.data.activity, pieScale(d.data.activity))
+  //       return pieScale(d.data.activity)
+  //     })
 
-  pies.append('path')
-    .attr('d', d => {
-      return arc(d)
-    })
-    .attr('fill', function (d, i) {
-      return pieScale(d.data.activity)
-    })
+  //   points = pieContainer.selectAll('.pie')
+  //     .data(sTimeList, d => d.country)
+  //     .attr('transform', (d) => {
+  //       const centroid = path.centroid(d.feature)
+  //       return `translate(${centroid[0]}, ${centroid[1]})`
+  //     })
+
+  //   let pies_enter = points.selectAll('.pie')
+  //     .data(d => {
+  //       let bounds = path.bounds(d.feature)
+  //       let max1 = Math.min(Math.abs(bounds[0][0] - bounds[1][0]), Math.abs(bounds[0][1] - bounds[1][1]))
+  //       let radius = Math.sqrt(path.area(d.feature))
+  //       console.log('projection', radius, max1, path.area(d.feature), d.feature.properties.NAME)
+  //       d.values.map(d => {
+  //         d.radius = 0.618 * radius / 2
+  //         return d
+  //       })
+
+  //       return pie(d.values)
+  //     })
+
+  // valide code
+
+  //   pies_enter.enter().append('path')
+  //     .attr('d', d => {
+  //       return arc(d)
+  //     })
+  //     .attr('fill', function (d, i) {
+  //       return pieScale(d.data.activity)
+  //     })
+
+  //   pies_enter
+  //     .exit()
+  //     .transition()
+  //     .duration(500)
+  //     .remove()
+  //   pies_enter
+  //     .transition()
+  //     .duration(1000)
+  //     .attr('fill', function (d) {
+  //       return pieScale(d.data.activity)
+  //     })
+  //     .attrTween('d', arcTween)
+
+  let zoom = d3.zoom().scaleExtent([1, 10]).on('zoom', zoomed)
+
+  d3.select('#map').call(zoom)
+}
+
+function zoomed () {
+  d3.select('#map').attr('transform',
+    'translate(' + d3.event.transform.x + ',' + d3.event.transform.y + ')scale(' + d3.event.transform.k + ')')
+
+  d3.select('#pies').attr('transform',
+    'translate(' + d3.event.transform.x + ',' + d3.event.transform.y + ')scale(' + d3.event.transform.k + ')')
 }
 
 //   // append labels
@@ -267,41 +346,80 @@ function createPieMap (activities) {
 //     })
 // }
 
+function updateLegend () {
+  var legend = d3.legendColor()
+    .labels(function ({
+      i,
+      genLength,
+      generatedLabels,
+      labelDelimiter
+    }) {
+      console.log('legend', i, genLength, generatedLabels, labelDelimiter)
+      if (i === 0) {
+        const values = generatedLabels[i].split(` ${labelDelimiter} `)
+        return `Less than ${nbToString(values[1])}`
+      } else if (i === genLength - 1) {
+        const values = generatedLabels[i].split(` ${labelDelimiter} `)
+        return `${nbToString(values[0])} or more`
+      } else {
+        const values = generatedLabels[i].split(` ${labelDelimiter} `)
+        console.log('legend', values)
+        return `${nbToString(values[0])} to ${nbToString(values[1])}`
+      }
+    })
+    .scale(scaleC)
+
+  d3.select('#legend')
+    .call(legend)
+}
+
 function updateMapScale (activity) {
   pTime = getParticipationTimeMap(activity)
   pRate = getParticipationRateMap(activity)
 
   console.log('updateMapScale()', 'Activity', activity, 'pTime', pTime)
 
-  let pTimeList = Object.keys(pTime).map(function (key) { return pTime[key] })
-  pTimeList.filter(Boolean)
+  let pTimeList = Object.keys(pTime).map(function (key) {
+    return pTime[key]
+  })
 
+  pTimeList.filter(Boolean)
   let minPTime = d3.min(pTimeList)
   let maxPTime = d3.max(pTimeList)
 
   console.log('updateScale():', 'Get new range:[', minPTime, maxPTime, ']')
 
-  scaleC = d3.scaleThreshold()
-    .domain(d3.range(9).map(function (d) { return minPTime + d * (maxPTime - minPTime) / 8 }))
+  scaleC = d3
+    .scaleThreshold()
+    .domain(
+      d3.range(9).map(function (d) {
+        return minPTime + (d * (maxPTime - minPTime)) / 8
+      })
+    )
     .range(colorbrewer['Blues'][9])
 }
 
 function updatePieMapScale (activities) {
   getSpentTimeList(activities)
-  let minSTime = d3.min(sTimeList, d => d.total)
+  let result = sTimeList.filter(t => t.total > 0.0)
+  let minSTime = d3.min(result, d => d.total)
   let maxSTime = d3.max(sTimeList, d => d.total)
 
   console.log('updateScale():', 'Get new range:[', minSTime, maxSTime, ']')
 
-  pieScale = d3.scaleOrdinal('schemeCategory20')
+  pieScale = d3
+    .scaleOrdinal('schemeCategory20')
     .domain(activities)
     .range(d3.schemeCategory20)
 
-  scaleC = d3.scaleThreshold()
-    .domain(d3.range(9).map(function (d) {
-      return minSTime + d * (maxSTime - minSTime) / 8
-    }))
-    .range(colorbrewer['YlGn'][9])
+  scaleC = d3
+    .scaleThreshold()
+    .domain(
+      d3.range(9).map(function (d) {
+        return minSTime + (d * (maxSTime - minSTime)) / 8
+      })
+    )
+    .range(colorbrewer['PuRd'][9])
 }
 
 function pRateTips (d) {
@@ -314,14 +432,14 @@ function pRateTips (d) {
   //
 }
 
-function updatePie (activites) {
-  const projection = d3.geoMercator()
-    // d3's 'fitSize' magically sizes and positions the map for you
-    .fitSize([width, height], mapData)
+// function updatePie (activites) {
+//   const projection = d3.geoMercator()
+//     // d3's 'fitSize' magically sizes and positions the map for you
+//     .fitSize([width, height], mapData)
 
-  // this is the function that generates position data from the projection
-  const path = d3.geoPath()
-    .projection(projection)
+//   // this is the function that generates position data from the projection
+//   const path = d3.geoPath()
+//     .projection(projection)
 
 //   pieContainer.selectAll('.pie_path')
 //     .data(pie(sTimeList['Belgium']), d => {
@@ -354,70 +472,69 @@ function updatePie (activites) {
 //       return color(i)
 //     })
 //     .attrTween('d', arcTween)
-}
+// }
 
-function createTimeSpentMap (activites) {
-  getSpentTimeList(activities)
-  updatePieMapScale(activites)
+// function createTimeSpentMap (activites) {
+//   getSpentTimeList(activities)
+//   updatePieMapScale(activites)
 
-  let pie = d3.pie()
-    .value(d => d.values.value)
+//   let pie = d3.pie()
+//     .value(d => d.values.value)
 
-  let outerRadius = 50
-  // Easy colors accessible via a 10-step ordinal scale
-  var color = d3.scaleOrdinal(d3.schemeCategory10)
+//   let outerRadius = 50
+//   // Easy colors accessible via a 10-step ordinal scale
+//   var color = d3.scaleOrdinal(d3.schemeCategory10)
 
-  var arc = d3.arc()
-    .innerRadius(0)
-    .outerRadius(d => d.data.radius)
+//   var arc = d3.arc()
+//     .innerRadius(0)
+//     .outerRadius(d => d.data.radius)
 
-  console.log(pie(sTimeList['Greece']))
+//   console.log(pie(sTimeList['Greece']))
 
-  let pie_set = pieContainer.selectAll('.pie')
-    .data(sTimeList, d => d.country)
-    .enter()
-    .append('g')
-    .attr('class', 'pie')
+//   let pie_set = pieContainer.selectAll('.pie')
+//     .data(sTimeList, d => d.country)
+//     .enter()
+//     .append('g')
+//     .attr('class', 'pie')
 
-  let arcs = pieContainer.selectAll('.arc')
-    .data(pie(sTimeList['Belgium']), d => d.data.values.activitiy)
-    .enter()
-    .append('g')
-    .attr('class', 'arc')
-    .attr('transform', 'translate(' + outerRadius + ', ' + outerRadius +
-        ')')
+//   let arcs = pieContainer.selectAll('.arc')
+//     .data(pie(sTimeList['Belgium']), d => d.data.values.activitiy)
+//     .enter()
+//     .append('g')
+//     .attr('class', 'arc')
+//     .attr('transform', 'translate(' + outerRadius + ', ' + outerRadius +
+//         ')')
 
-  arcs.append('path')
-    .attr('fill', function (d, i) {
-      return color(i)
-    })
-    .attr('d', arc)
-    .attr('class', 'pie_path')
+//   arcs.append('path')
+//     .attr('fill', function (d, i) {
+//       return color(i)
+//     })
+//     .attr('d', arc)
+//     .attr('class', 'pie_path')
 
-  pieContainer.selectAll('.pie_path')
-    .data(pie(sTimeList['Belgium']), d => {
-      console.log(d.data.values.activitiy)
-      return d.data.values.activitiy
-    })
-    .transition()
-    .duration(2000)
-    .attr('fill', function (d, i) {
-      return color(i)
-    })
-    .attrTween('d', arcTween)
-}
+//   pieContainer.selectAll('.pie_path')
+//     .data(pie(sTimeList['Belgium']), d => {
+//       console.log(d.data.values.activitiy)
+//       return d.data.values.activitiy
+//     })
+//     .transition()
+//     .duration(2000)
+//     .attr('fill', function (d, i) {
+//       return color(i)
+//     })
+//     .attrTween('d', arcTween)
+// }
 
-function arcTween (a) {
-  let arc = d3.arc()
-    .innerRadius(25)
-    .outerRadius(50)
-  var i = d3.interpolate(this._current, a)
-  this._current = i(0)
-  return function (t) {
-    return arc(i(t))
-  }
+function arcTween (d, arc) {
+  d.innerRadius = 0
+  var i = d3.interpolate({ startAngle: 0, endAngle: 0 }, d)
+  return function (t) { return arc(i(t)) }
 }
 
 function isArray (value) {
   return value && typeof value === 'object' && value.constructor === Array
+}
+
+function nbToString (min) {
+  return d3.format('02d')(Math.floor(min / 60)) + 'H' + d3.format('02d')(min % 60)
 }
--- js/load-tim.js
+++ js/load-tim.js
@@ -1,50 +1,48 @@
-var countries = [];
-var activities = [];
-var dataset = new Map();
+var countries = []
+var activities = []
+var dataset = new Map()
 
 //exemple d'accès au dataset : dataset.get("Belgium").get("Sleep").participationTime ; dataset.get("Belgium").get("Sleep").participationRate ; dataset.get("Belgium").get("Sleep").timeSpent
 
-d3.csv("data/label.csv", function (label) {
+d3.csv('data/label.csv', function (label) {
   for (var i = 13; i < 34; i++) {
-    countries.push(label[i]["DATASET: Time spent, participation time and participation rate in the main activity by sex and day of the week [tus_00week]"]);
+    countries.push(label[i]['DATASET: Time spent, participation time and participation rate in the main activity by sex and day of the week [tus_00week]'])
   };
   for (var i = 46; i < 101; i++) {
-    activities.push(label[i]["DATASET: Time spent, participation time and participation rate in the main activity by sex and day of the week [tus_00week]"]);
+    activities.push(label[i]['DATASET: Time spent, participation time and participation rate in the main activity by sex and day of the week [tus_00week]'])
   };
 
   for (var i = 0; i < countries.length; i++) {
-    var map = new Map();
+    var map = new Map()
     for (var j = 0; j < activities.length; j++) {
-      var objet = { participationTime: 0, participationRate: 0, timeSpent: 0 };
-      map.set(activities[j], objet);
+      var objet = { participationTime: 0, participationRate: 0, timeSpent: 0 }
+      map.set(activities[j], objet)
     }
-    dataset.set(countries[i], map);
+    dataset.set(countries[i], map)
   }
-});
+})
 
-d3.csv("data/data.csv", function (error, data) {
+d3.csv('data/data.csv', function (error, data) {
   for (var i = 0; i < data.length; i++) {
     l = data[i]
-    if ((l.DAYSWEEK === "All days of the week") && (l.SEX === "Total") && (l.ACL00 != "Total")) {
-      if (l.UNIT === "Time spent (hh:mm)") {
-        dataset.get(l.GEO).get(l.ACL00).timeSpent = l.Value;
+    if ((l.DAYSWEEK === 'All days of the week') && (l.SEX === 'Total') && (l.ACL00 != 'Total')) {
+      if (l.UNIT === 'Time spent (hh:mm)') {
+        dataset.get(l.GEO).get(l.ACL00).timeSpent = l.Value
       }
-      if (l.UNIT === "Participation time (hh:mm)") {
-        dataset.get(l.GEO).get(l.ACL00).participationTime = l.Value;
+      if (l.UNIT === 'Participation time (hh:mm)') {
+        dataset.get(l.GEO).get(l.ACL00).participationTime = l.Value
       }
 
-      if (l.UNIT === "Participation rate (%)") {
-        dataset.get(l.GEO).get(l.ACL00).participationRate = l.Value;
+      if (l.UNIT === 'Participation rate (%)') {
+        dataset.get(l.GEO).get(l.ACL00).participationRate = l.Value
       }
     }
   }
-	create_list_activities_ts();
-	create_list_activities_pt();	
-	createNewGRAPHsvg();
+  create_list_activities_ts()
+	create_list_activities_pt()	
+	createNewGRAPHsvg()
 	
-	$("#participation_time_li").trigger("click");
-	plotGraphActivity(activities[0]);
-	$("#time_spent_li").trigger("click");
-});
-
-
+	$('#participation_time_li').trigger('click')
+	plotGraphActivity(activities[0])
+	$('#time_spent_li').trigger('click')
+})
